b1cb7a94707893687847f4105962f216fd665603
Moona Siddiqui <143139723+MoonaSiddiqui@users.noreply.github.com>
2026-08-25 14:42:46 +0000

    Add ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,1 @@
+ module.exports = [ { files: ["**/*.js"], languageOptions: { ecmaVersion: "latest", sourceType: "commonjs", globals: { require: "readonly", module: "readonly", process: "readonly", console: "readonly", __dirname: "readonly" } }, rules: {} } ];
